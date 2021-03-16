4cdc3df36e564b4b787603a8ea399d6738eb1585
Monika <80641920+monk200@users.noreply.github.com>
2021-03-15 20:19:32 -0500

    Complete lab_intro.cpp and pass test cases
    Lines 67 and 69 of createSpotlight function throw warnings because they compare an unsigned int to a signed int

--- lab_intro/lab_intro.cpp
+++ lab_intro/lab_intro.cpp
@@ -45,7 +45,7 @@ PNG grayscale(PNG image) {
  * is a total of `sqrt((3 * 3) + (4 * 4)) = sqrt(25) = 5` pixels away and
  * its luminance is decreased by 2.5% (0.975x its original value).  At a
  * distance over 160 pixels away, the luminance will always decreased by 80%.
- * 
+ *
  * The modified PNG is then returned.
  *
  * @param image A PNG object which holds the image data to be modified.
@@ -55,11 +55,29 @@ PNG grayscale(PNG image) {
  * @return The image with a spotlight.
  */
 PNG createSpotlight(PNG image, int centerX, int centerY) {
+  // loops through image
+  for (unsigned x=0; x < image.width(); x++) {
+    for (unsigned y=0; y < image.height(); y++) {
+      HSLAPixel & pixel = image.getPixel(x, y);   // pixel is a pointer to the memory stored inside of png image -> you are changing the image directly
+      double lum = pixel.l;
+
+      // get distance between center and pixel, accounts for all quadrents around the given center
+      // !!! WARNINGS THROWN HERE BECAUSE X AND Y ARE UNSIGNED WHILE CENTERX AND CENTERY ARE INT !!!
+      int dx, dy;
+      if (x < centerX) dx = centerX - x;
+      else dx = x - centerX;
+      if (y < centerY) dy = centerY - y;
+      else dy = y - centerY;
+
+      double distanceFromCenter = sqrt(pow(dx, 2) + pow(dy, 2));    // euclidean distance of pixel from the center points given
+      if (distanceFromCenter < 160) pixel.l = lum * (1 - (distanceFromCenter * 0.005));   // pixel loses 0.5% luminance per 1 pixel distanceFromCenter
+      else pixel.l = lum * 0.2;   // if farther than 160 pixels, pixel loses 80% luminance
+    }
+  }
 
   return image;
-  
 }
- 
+
 
 /**
  * Returns a image transformed to Illini colors.
@@ -72,10 +90,27 @@ PNG createSpotlight(PNG image, int centerX, int centerY) {
  * @return The illinify'd image.
 **/
 PNG illinify(PNG image) {
+  // loops through image
+  for (unsigned x=0; x < image.width(); x++) {
+    for (unsigned y=0; y < image.height(); y++) {
+      HSLAPixel & pixel = image.getPixel(x, y);   // pixel is a pointer to the memory stored inside of png image -> you are changing the image directly
+      double hue = pixel.h;
+
+      // determine distance from 11 and distance from 216
+      double distToOrange = 360;
+      if (hue < (11+180)) distToOrange = abs(hue - 11);   // distance for hues to the right of orange by <=180
+      else distToOrange = (360-hue) + 11;   // accounts for wraparound
+      double distToBlue = abs(hue - 216);   // wraparound doesn't need to be considered here because anything near 0/360 will be closest to orange
+
+      // sets new hue for pixel
+      if (distToOrange < distToBlue) pixel.h = 11;
+      else pixel.h = 216;
+    }
+  }
 
   return image;
 }
- 
+
 
 /**
 * Returns an immge that has been watermarked by another image.
@@ -90,6 +125,25 @@ PNG illinify(PNG image) {
 * @return The watermarked image.
 */
 PNG watermark(PNG firstImage, PNG secondImage) {
+  // determine bounds
+  unsigned width, height;
+  if (firstImage.width() > secondImage.width()) width = firstImage.width();
+  else width = secondImage.width();
+  if (firstImage.height() > secondImage.height()) height = firstImage.height();
+  else height = secondImage.height();
+
+  // loop through only the pixels where the images overlap
+  for (unsigned x=0; x < width; x++) {
+    for (unsigned y=0; y < height; y++) {
+      // pixel is a pointer to the memory stored inside of png image -> you are changing the image directly
+      HSLAPixel & firstPixel = firstImage.getPixel(x, y);
+      HSLAPixel & secondPixel = secondImage.getPixel(x, y);
+
+      if (secondPixel.l == 1) {   // if the luminance of the second image is 1
+        if (firstPixel.l <= 0.8) firstPixel.l += 0.2;   // pixel at same location has its luminance incrased by 0.2, but not exceeding 1
+      }
+    }
+  }
 
   return firstImage;
 }
